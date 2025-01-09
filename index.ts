bd81e836e37bb14609a82cc922a666883441f011
Alkan Aziz <alkanaziz@gmail.com>
2025-01-09 20:44:08 +0100

    Implement runLLM invocation in index.ts to process user messages

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { runLLM } from './src/llm'
 
 const userMessage = process.argv[2]
 
@@ -6,3 +7,18 @@ if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+// runLLM({ userMessage })
+//   .then(console.log)
+//   .catch(console.error)
+
+const run = async () => {
+  try {
+    const response = await runLLM({ userMessage })
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+run()
