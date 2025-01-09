75c4b4484de3a2822c837a62e180c35ef2e49749
Alkan Aziz <alkanaziz@gmail.com>
2025-01-09 23:23:44 +0100

    refactor runLLM to accept an array of AIMessage objects

--- src/llm.ts
+++ src/llm.ts
@@ -1,11 +1,13 @@
+import type { AIMessage } from '../types'
 import { openai } from './ai'
 
-export const runLLM = async ({ userMessage }: { userMessage: string }) => {
+export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages: [{ role: 'user', content: userMessage }],
+    messages,
   })
 
+  console.log({response, message: response.choices[0].message})
   return response.choices[0].message.content
 }
